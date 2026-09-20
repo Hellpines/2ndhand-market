85c9b0c5835fe0cfefb7f191511390b0dc8932d0
Hellpines <tolstenkov.ilya@mail.ru>
2026-09-20 19:58:23 +0300

    fix: edit config files for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,10 @@ module.exports = (env, argv) => {
             options: {
               presets: [
                 '@babel/preset-env',
-                ['@babel/preset-react', { runtime: 'automatic' }],
+                ['@babel/preset-react', { 
+                  runtime: 'automatic',
+                  development: !isProduction,
+                }],
               ],
             },
           },
